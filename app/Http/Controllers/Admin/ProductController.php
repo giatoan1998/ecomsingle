03dfb18d098c1f89ba029adabebf976ae5830a0e
toan.dg <toandinhgia@tcom.vn>
2023-07-28 00:17:09 +0700

    add part 5

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -11,7 +11,8 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     public function Index() {
-        return view('admin.allproduct');
+        $products = Product::latest()->get();
+        return view('admin.allproduct', compact('products'));
     }
 
     public function AddProduct() {
@@ -62,5 +63,68 @@ class ProductController extends Controller
         Subcategory::where('id', $subcategory_id)->increment('product_count', 1);
 
         return redirect()->route('allproducts')->with('message', 'Thêm thành công product.');
+    }
+
+    public function EditProductImg($id) {
+        $productinfo = Product::findOrFail($id);
+
+        return view('admin.editproductimg', compact('productinfo'));
+    }
+
+    public function UpdateProductImg(Request $request) {
+        $request->validate([
+            'product_img' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        $id = $request->id;
+        $image = $request->file('product_img');
+        $img_name = hexdec(uniqid()).'.'. $image->getClientOriginalExtension();
+        $request->product_img->move(public_path('upload'), $img_name);
+        $img_url = 'upload/' . $img_name;
+
+        Product::findOrFail($id)->update([
+            'product_img' => $img_url,
+        ]);
+
+        return redirect()->route('allproducts')->with('message', 'Cập nhật hình ảnh thành công.');
+    }
+
+    public function EditProduct($id) {
+        $productinfo = Product::findOrfail($id);
+
+        return view('admin.editproduct', compact('productinfo'));
+    }
+
+    public function UpdateProduct(Request $request) {
+        $productid = $request->id;
+
+        $request->validate([
+            'product_name' => 'required|unique:products',
+            'price' => 'required',
+            'quantity' => 'required',
+            'product_short_des' => 'required',
+            'product_long_des' => 'required',
+        ]);
+
+        Product::findOrFail($productid)->update([
+            'product_name' => $request->product_name,
+            'product_short_des' => $request->product_short_des,
+            'product_long_des' => $request->product_long_des,
+            'price' => $request->price,
+            'quantity' => $request->quantity,
+            'slug' => strtolower(str_replace(' ', '-', $request->product_name)),
+        ]);
+
+        return redirect()->route('allproducts')->with('message', 'Cập nhật thông tin sản phẩm thành công.');
+    }
+
+    public function DeleteProduct($id) {
+        $cat_id = Product::where('id', $id)->value('product_category_id');
+        $subcat_id = Product::where('id', $id)->value('product_subcategory_id');
+        Product::findOrFail($id)->delete();
+        Category::where('id', $cat_id)->decrement('product_count', 1);
+        Subcategory::where('id', $subcat_id)->decrement('product_count', 1);
+        
+        return redirect()->route('allproducts')->with('message', 'Xóa sản phẩm thành công.');
     }
 }
