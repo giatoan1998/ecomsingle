392efed2c7b3ff741cd418464029dfa73e8b0565
toan.dg <toandinhgia@tcom.vn>
2023-07-29 17:24:15 +0700

    add part 10

---
+++ resources/views/user_template/newrelease.blade.php
@@ -0,0 +1,4 @@
+@extends('user_template.layouts.template')
+@section('main-content')
+<h2>New release page</h2>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\SubCategoryController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,8 +21,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(HomeController::class)->group(function () {
+    Route::get('/', 'Index')->name('home');
+});
+
+Route::controller(ClientController::class)->group(function () {
+    Route::get('/category{id}/{slug}', 'Category')->name('category');
+    Route::get('/single-product', 'SingleProduct')->name('singleproduct');
+    Route::get('/add-to-cart', 'AddToCart')->name('addtocart');
+    Route::get('/check-out', 'CheckOut')->name('checkout');
+    Route::get('/user-profile', 'UserProfile')->name('userprofile');
+
+    Route::get('/new-release', 'NewRelease')->name('newrelease');
+    Route::get('/todays-deal', 'TodaysDeal')->name('todaysdeal');
+    Route::get('/customer-service', 'CustomerService')->name('customerservice');
+
 });
 
 Route::get('/dashboard', function () {
@@ -39,7 +54,6 @@ Route::middleware(['auth', 'role:admin'])->group(function(){
         Route::get('/admin/edit-category/{id}', 'EditCategory')->name('editcategory');
         Route::post('/admin/update-category', 'UpdateCategory')->name('updatecategory');
         Route::get('/admin/delete-category/{id}', 'DeleteCategory')->name('deletecategory');
-
     });
 
     Route::controller(SubCategoryController::class)->group(function(){
@@ -57,11 +71,8 @@ Route::middleware(['auth', 'role:admin'])->group(function(){
         Route::post('/admin/store-product', 'StoreProduct')->name('storeproduct');
         Route::get('/admin/edit-product-img/{id}', 'EditProductImg')->name('editproductimg');
         Route::post('/admin/update-product-img', 'UpdateProductImg')->name('updateproductimg');
-
         Route::get('/admin/edit-product/{id}', 'EditProduct')->name('editproduct');
-
         Route::post('/admin/update-product', 'UpdateProduct')->name('updateproduct');
-
         Route::get('/admin/delete-product/{id}', 'DeleteProduct')->name('deleteproduct');
     });
 
